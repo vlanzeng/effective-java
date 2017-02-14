0c25ef803dfa54340b42f4cb0e620fdad3401f39
monalisa <wzbbbx@sina.com>
2017-02-14 09:45:44 +0800

    1:新增超类，子类，构造器中调用方法造成异常 1#	修改：     src/com/effective/java/concurrency/Cache.java

---
+++ src/com/effective/java/abstractest/Sub.java
@@ -0,0 +1,24 @@
+package com.effective.java.abstractest;
+
+import java.util.Date;
+
+public final  class Sub extends Super {
+	
+	private final Date date;
+	
+	public  Sub(){
+		date =new Date();
+	}
+	
+	@Override
+	 public void overrideMe() {
+		System.out.println(date);
+	 }
+	
+	
+	public static void main(String[] args) {
+		Sub sub = new Sub();
+		sub.overrideMe();
+	}
+	
+}
---
+++ src/com/effective/java/abstractest/Super.java
@@ -0,0 +1,15 @@
+package com.effective.java.abstractest;
+
+public class Super {
+	
+	public Super() {
+		overrideMe();
+	}
+	
+	public void overrideMe() {
+		
+		
+	}
+	
+	
+}
